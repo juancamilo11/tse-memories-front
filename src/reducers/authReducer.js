9e41ce97ea91d6403fcb1816460f03f5159e9ccc
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-30 23:39:13 -0500

    Se agregaron los reducers para la auth y la UI y sus types

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,37 @@
+import types from "../types/types";
+
+/*
+//State description
+
+  if(logged) { //Google will provide us this information 
+    uid: U893LEID94KE782KES03,
+    name: Juan Camilo Cardona Calderón,
+    email: juancamilo19997814@gmail.com
+    photoUrl: https://lh3.googleusercontent.com/a-/AOh14GjnkTKE1MwBx1jBXLj6SCsCSUANvgmn28L0yh31wg=s96-c-rg-br100,
+    lastSignInTime: 1640235253245 
+  }
+
+  if(!logged) -> {} //Empty object (Initial State)
+*/
+
+const initialState = {};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.authLogin:
+      return {
+        ...state,
+        uid: action.payload.uid,
+        name: action.payload.displayName,
+        email: action.payload.email,
+        photoUrl: action.payload.photoUrl,
+        lastSignInTime: action.payload.lastSignInTime,
+      };
+    case types.authLogout:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
---
+++ src/types/types.js
@@ -0,0 +1,13 @@
+const types = {
+  //Action types for Login / Logout
+  authLogin: "[auth] login",
+  authLogout: "[auth] logout",
+
+  //Action types for UI events
+  uiSetError: "[UI] Set Error",
+  uiRemoveError: "[UI] Remove Error",
+  uiStartLoading: "[UI] Start Loading",
+  uiFinishLoading: "[UI] Finish Loading",
+};
+
+export default types;
