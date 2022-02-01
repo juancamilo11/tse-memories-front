79d0ea6587748738b528ca97774b235ec5d9f59f
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-31 23:58:09 -0500

    Se agregó la vista de Login con estilos

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
       <div className="loader__loader-content">
         <h1 className="loader__loading-title">Cargando...</h1>
         <img
-          src={process.env.PUBLIC_URL + "/assets/img/login/loading.svg"}
+          src={process.env.PUBLIC_URL + "/assets/img/loader/loading.svg"}
           className="loading__loader"
           alt="loading"
         />
--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,9 +1,71 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { startGoogleLogin } from "../../actions/authActions";
 
 const LoginPage = () => {
+  const dispatch = useDispatch();
+
+  const handleLoginWithGoogle = (e) => {
+    e.preventDefault();
+    dispatch(startGoogleLogin());
+  };
+
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="login__main-container">
+      <div className="login__sidebar">
+        <div className="login__welcome-section">
+          <img
+            src={
+              process.env.PUBLIC_URL + "/assets/img/login/login-sidebar-img.png"
+            }
+            className="login__welcome-section-img"
+            alt="logo"
+          />
+          <h2 className="login__welcome-section-title">
+            TAKE IT
+            <br />
+            SHARE IT
+            <br />
+            ENJOY IT
+          </h2>
+        </div>
+        <div className="login__login-buttons-section">
+          <button
+            onClick={handleLoginWithGoogle}
+            className="login__button-login login__button-google"
+          >
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/assets/img/login/google-img-button.png"
+              }
+              className="login__login-buttons-img"
+              alt="google-login"
+            />
+            <span className="login__login-button-text">
+              Ingresar con Google
+            </span>
+          </button>
+        </div>
+      </div>
+      <div
+        className="login__welcome-img-container"
+        style={{
+          backgroundImage: `url(${
+            process.env.PUBLIC_URL + "/assets/img/login/login-main-page.jpg"
+          })`,
+        }}
+      >
+        <section className="login__app-description">
+          <h2 className="login__app-description-title">TSE Memories</h2>
+          <h4 className="login__app-description-title">
+            ¡Crea recuerdos y compartelos con quién quieras!
+          </h4>
+          <h6 className="login__app-description-title">
+            ¡Ingresa y crea un mundo lleno de recuerdos!
+          </h6>
+        </section>
+      </div>
     </div>
   );
 };
